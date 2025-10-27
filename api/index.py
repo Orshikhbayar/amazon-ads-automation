4aedb92f87b506eb0888a940c4bb49dc207fe928
DDAM-M0089 <ddam-m0089@DDAM-M0089.local>
2025-10-27 10:50:24 +0800

    Ensure api/index.py shim

--- api/index.py
+++ api/index.py
@@ -1,6 +1,4 @@
 # api/index.py
 import os, sys
-# add project root to import path
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-from app import app  # re-use your existing Flask app from app.py
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))  # add project root
+from app import app  # reuse your Flask app object
